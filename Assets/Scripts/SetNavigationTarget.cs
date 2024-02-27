9a8a7cc592768ce6da790155ac940d5d09bb1198
Nicolas Werner <64469511+nicolas-werner@users.noreply.github.com>
2024-02-27 23:59:56 +0100

    dropdowntest

--- Assets/Scripts/SetNavigationTarget.cs
+++ Assets/Scripts/SetNavigationTarget.cs
@@ -2,37 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
 public class SetNavigationTarget : MonoBehaviour
 {
  
     [SerializeField]
-    private Camera topDownCamera;
+    private TMP_Dropdown navigationTargetDropDown;
+    [SerializeField]
+    private List<Target> navigationTargetObjects = new List<Target>();
 
-     [SerializeField]
-     private GameObject navTargetObject;
+    private NavMeshPath path;
+    private LineRenderer line;
+    private Vector3 targetPosition = Vector3.zero;
 
-     private NavMeshPath path;
-     private LineRenderer line;
+    private bool lineToggle = false;
 
-    void Start()
-    {
+    private void Start() {
         path = new NavMeshPath();
         line = transform.GetComponent<LineRenderer>();
-        
+        line.enabled = lineToggle;
     }
 
-    // Update is called once per frame
-void Update()
-{
-    NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, NavMesh.AllAreas, path);
-    line.positionCount = path.corners.Length;
-    line.SetPositions(path.corners);
-    line.enabled = true;
 
-    // Calculate and print the length of the path
-    float pathLength = CalculatePathLength(path);
-    Debug.Log("Path Length: " + pathLength);
-}
+    private void Update() {
+        if (lineToggle && targetPosition != Vector3.zero) {
+            NavMesh.CalculatePath(transform.position, targetPosition, NavMesh.AllAreas, path);
+            line.positionCount = path.corners.Length;
+            line.SetPositions(path.corners);
+            // Calculatea and prints the length of the path
+            float pathLength = CalculatePathLength(path);
+            Debug.Log("Path Length: " + pathLength);
+        }
+    }
+
+
+    public void SetCurrentNavigationTarget(int selectedValue) {
+        targetPosition = Vector3.zero;
+        string selectedText = navigationTargetDropDown.options[selectedValue].text;
+        Target currentTarget = navigationTargetObjects.Find(x => x.Name.Equals(selectedText));
+        if (currentTarget != null) {
+            targetPosition = currentTarget.PositionObject.transform.position;
+        }
+    }
+
+    public void ToggleVisibility() {
+        lineToggle = !lineToggle;
+        line.enabled = lineToggle;
+    }
+
 
 float CalculatePathLength(NavMeshPath path)
 {
@@ -47,5 +64,4 @@ float CalculatePathLength(NavMeshPath path)
 
     return length;
 }
-
 }
